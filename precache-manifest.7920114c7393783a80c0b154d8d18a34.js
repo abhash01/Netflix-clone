860fadec6454ebe0ee4b1d33e062d764232a3961
abhash01 <abhash01@gmail.com>
2020-10-19 23:40:49 +0530

    Updates

---
+++ precache-manifest.7920114c7393783a80c0b154d8d18a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc634a1067ab5543cddf50dc1597bb61",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "c8062e7bb7ea30e2859e",
+    "url": "/Netflix-clone/static/css/main.92e1995a.chunk.css"
+  },
+  {
+    "revision": "4c701943a2c15051c75b",
+    "url": "/Netflix-clone/static/js/2.bf6e9201.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-clone/static/js/2.bf6e9201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8062e7bb7ea30e2859e",
+    "url": "/Netflix-clone/static/js/main.65a97078.chunk.js"
+  },
+  {
+    "revision": "1f70fcb38d92599a6c08",
+    "url": "/Netflix-clone/static/js/runtime-main.d8bb3d81.js"
+  }
+]);
